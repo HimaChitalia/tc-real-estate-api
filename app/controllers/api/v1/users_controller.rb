efaf078f9b39b3fedaa4b866c6fed65c6932933c
Hima <hima_chhag@yahoo.com>
2017-08-23 11:57:51 -0400

    api/v1/users route tested

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,19 @@
 class Api::V1::UsersController < ApplicationController
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       render 'users/user_with_token.json.jbuilder', user: @user
-      
     else
       render json: {
         errors: @user.errors
-      }, status: 500    
+      }, status: 500
     end
   end
-  
+
   private
 
     def user_params
-      params.require(:user).permit(:email, :username, :password)
+      params.fetch(:user, {}).permit(:email, :username, :password)
     end
 end
