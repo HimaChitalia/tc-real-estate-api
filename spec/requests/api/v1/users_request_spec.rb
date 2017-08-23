1f01d6fa4c48c814f920d2199212558da7b5d0fb
Hima <hima_chhag@yahoo.com>
2017-08-23 12:13:20 -0400

    Passing api/v1/users test

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -26,13 +26,40 @@ RSpec.describe "Api::V1:Users", type: :request do
         expect(User.all.count).to eq(1)
       end
 
-      it "returns the new user and a JWT Token"
+      it "returns the new user and a JWT Token" do
+        body = JSON.parse(@response.body)
+
+         expect(@response.status).to eq(200)
+         expect(body['user']['id']).not_to eq(nil)
+         expect(body['user']['email']).to eq('b@baani.com')
+         expect(body['user']['password_digest']).to eq(nil)
+         expect(body['token']).not_to eq(nil)
+      end
+
     end
 
     describe "on error" do
 
-      it "required a valid email or password" do
+      it "required a valid email, usename and password" do
+        params = {
+          user: {
+            email: '',
+            password: ''
+          }
+        }
 
+        post "/api/v1/users",
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(body["errors"]).to eq({
+          "password"=>["can't be blank"],
+          "email"=>["can't be blank", "is invalid"],
+          "username" => ["can't be blank"]
+        })
       end
     end
 
