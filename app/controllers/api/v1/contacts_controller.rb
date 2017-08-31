38efa780d7d7e5f1412bff70961098aef0faedad
Hima <hima_chhag@yahoo.com>
2017-08-31 16:11:58 -0400

    Removed binding and commented code

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,21 +4,11 @@ class Api::V1::ContactsController < ApplicationController
   def create
       @contact = Contact.new(params[:contact])
       @contact.request = request
-      binding.pry
       if @contact.deliver
         render json: {message: 'Thank you for your message. We will contact you soon!'}
       else
-        binding.pry
         render json: {message: 'Cannot send message.'}
       end
     end
 
-
-
-  private
-
-
-    # def house_params
-    #   params.fetch(:house, {}).permit(:address, :city, :state, :zip, :latitude, :longitude, :user_id)
-    # end
 end
