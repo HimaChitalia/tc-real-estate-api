fa97422f02d79da97a709df5d330749427d9008a
Hima <hima_chhag@yahoo.com>
2017-08-31 10:09:44 -0400

    Adding origins for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://trushal-chitalia.herokuapp.com', 'http://trushal-chitalia.herokuapp.com'
 
     resource '*',
       headers: :any,
