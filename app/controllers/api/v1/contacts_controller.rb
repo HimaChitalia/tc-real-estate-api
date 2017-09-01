ec84fde9e48e31c88791bfcc24b9cdcc3586ce80
Hima <hima_chhag@yahoo.com>
2017-08-31 21:31:14 -0400

    modified controller to allow to send email through frontend

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ContactsController < ApplicationController
 
-
   def create
-      @contact = Contact.new(params[:contact])
+      @contact = Contact.new(params[:contact][:data])
+      name = params[:contact][:data][:name]
       @contact.request = request
       if @contact.deliver
-        render json: {message: 'Thank you for your message. We will contact you soon!'}
+        render json: {message: "Thank you for your Email, #{name.capitalize}. I will get back to you shortly!"}
       else
         render json: {message: 'Cannot send message.'}
       end
